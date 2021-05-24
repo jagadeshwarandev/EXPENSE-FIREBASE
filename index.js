8bd269975c317d20e43e424ab95dfd98a752e2ae
jagadeshwaran <76894794+jagadeshwarandev@users.noreply.github.com>
2021-05-24 12:15:37 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,86 +1,98 @@
-const inputElement = document.querySelector("#inputAmount");
-const element = document.querySelector("#btnAddExpense");
-const headingEl = document.querySelector("#headingTotal");
-const inputDescEl = document.querySelector("#inputDesc");
-const expenseTableEl = document.querySelector("#expenseTable");
-const alertEl = document.querySelector("#alert");
-
-let totalExpense = 0;
-
-headingEl.textContent = totalExpense;
-
-function addExpenseToTotal() {
-  const expenseItem = {};
-  const textAmount = inputElement.value;
-  const textDesc = inputDescEl.value;
-
-  const expense = parseInt(textAmount, 10);
-
-  expenseItem.desc = textDesc;
-  expenseItem.amount = expense;
-  expenseItem.moment = new Date();
-  if (!isNaN(expense)) {
-    addExpenseOnFirestore(textDesc, expense);
-    inputElement.value = "";
+ // Get element / reference
+ const headingEl = document.querySelector("#headingTotal");
+ const inputDescEl = document.querySelector("#inputDesc");
+ const inputEl = document.querySelector("#inputAmount");
+ const expenseTableEl = document.querySelector("#expenseTable");
+ const element = document.querySelector("#btnAddExpense");
+ let totalExpense = 0;
+ headingEl.textContent = `Total: ${totalExpense}`;
+ // callback-fun
+ function addExpenseToTotal() {
+   const expenseItem = {};
+   const textAmount = inputEl.value;
+   const textDesc = inputDescEl.value;
+   const datetime= new Date();
+   const expense = parseInt(textAmount, 10);
+   if (textDesc !== "" && !isNaN(expense) && expense > 0) {
+     expenseItem.desc = textDesc;
+     expenseItem.amount = expense;
+     expenseItem.moment = datetime;
+    addExpenseOnFirestore(textDesc, expense, datetime);
+    inputEl.value = "";
     inputDescEl.value = "";
-  }
-  totalExpense += expense;
-  updateTotal();
-  allExpenses.push(expenseItem);
-  renderlist(allExpenses);
-  inputEl.value = "";
-  inputDescEl.value = "";
+   }
+   totalExpense += expense;
+   updateTotal();
+ }
+ // event-listner
+ element.addEventListener("click", addExpenseToTotal, false);
+ 
+ getDocuments();
+
+ function myFunction() {
+    db.collection("expenses").doc("total").get().then((doc) => {
+        let someText = `Total: ${doc.data().total}`;
+        headingEl.textContent = someText;
+       })
 }
 
-function updateTotal() {
-  let someText = `Total: ${totalExpense}`;
-  headingEl.textContent = someText;
-}
+window.onload = myFunction();
 
-// Control functions
-function getDateString(momento) {
-  return momento.toDate().toLocaleDateString("en-US", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
-}
 
-function deleteItem(docId, amount) {
-  deleteFromFirebase(docId, amount);
-}
+ // functions
+ function getDateString(momento) {
+   return momento.toLocaleDateString("en-US", {
+     year: "numeric",
+     month: "long",
+     day: "numeric"
+   });
+ }
 
-function renderList(arrOfList) {
-  //   console.log(arrOfList);
-  const allExpenseHTML = arrOfList.map((expense) => createListItem(expense));
-  const joinedAllExpenseHTML = allExpenseHTML.join("");
-  expenseTableEl.innerHTML = joinedAllExpenseHTML;
-}
-// renderList(allExpenses);
-element.addEventListener("click", addExpenseToTotal, false);
-getDocuments();
+ function updateTotal() {
+   let someText = `Total: ${totalExpense}`;
+   headingEl.textContent = someText;
+ }
 
-// View layer
-function createListItem({ desc, amount, createdAt, docId }) {
-  //   console.log(docId);
-  return `
-      <li class="list-group-item d-flex justify-content-between">
-      <div class="d-flex flex-column">
-      ${desc}
-          <small class="text-muted">${getDateString(createdAt)}</small>
-      </div>
-      <div>
-          <span class="px-5">
-          ${amount}
-          </span>
-          <button 
-            type="button" 
-            class="btn btn-outline-danger btn-sm"
-            onClick="deleteItem('${docId}', ${amount})"
-          >
-            <i class="fas fa-trash"></i>
-          </button>
-      </div>
-    </li>
-      `;
-}
+ function deleteItem(docId, expense) {
+    deleteFromFirebase(docId, expense);
+    totalExpense -= expense;
+    updateTotal();
+ }
+
+ function renderlist(arrOfList) {
+   const allExpensesHTML = arrOfList.map(expense =>
+     createListItem(expense)
+   );
+   const joinedAllExpenseHTML = allExpensesHTML.join("");
+   expenseTableEl.innerHTML = joinedAllExpenseHTML;
+//    allExpenses = arrOfList;
+ }
+ // view-layer-tabledetails
+ function createListItem({
+   desc,
+   amount,
+   docId,
+ }) {
+   return `
+             <li class="list-group-item d-flex justify-content-between">
+                     <div class="d-flex flex-column">
+                             ${desc}
+                            
+                     </div>
+                     <div>
+                         <span class="px-5">
+                                 ${amount}
+                         </span>
+                         <button type="button" class="btn btn-outline-danger btn-sm" onclick="deleteItem('${docId}', ${amount})">
+                             <i class="fas fa-trash-alt"></i>
+                         </button>
+                     </div>
+                 </li>
+             `;
+             
+ }
+
+
+//  <small class="text-muted">${
+//     datetime
+//   }</small>
